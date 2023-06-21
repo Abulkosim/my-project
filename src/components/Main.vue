e36eea5224f588be0a1832f6f9bef0f4f2cfff64
Abulkosim Kataev <abulkasimkattaev@gmail.com>
2023-06-21 10:34:11 +0500

    cards section

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,6 @@
 <script setup>
 import GetStarted from './GetStarted.vue'
-import UserFriendly from './UserFriendly.vue'
+import UserFriendlyCards from './UserFriendlyCards.vue'
 import AdvancedFeatures from './AdvancedFeatures.vue'
 import Feedback from './Feedback.vue'
 import Cards from './Cards.vue'
@@ -9,7 +9,7 @@ import Cards from './Cards.vue'
 <template>
   <main class="text-grayish">
     <GetStarted></GetStarted>
-    <UserFriendly></UserFriendly>
+    <UserFriendlyCards></UserFriendlyCards>
     <AdvancedFeatures></AdvancedFeatures>
     <Cards></Cards>
     <Feedback></Feedback>
---
+++ src/components/UserFriendlyCards.vue
@@ -0,0 +1,48 @@
+<script setup>
+const list = [
+  {
+    title: "User friendly",
+    text: "A pseudo-Latin text used in web design, layout, and printing in place of things to emphasise design.",
+    img: "https://preview.cruip.com/storm/images/feature-tile-icon-01.svg"
+  },
+  {
+    title: "User friendly",
+    text: "A pseudo-Latin text used in web design, layout, and printing in place of things to emphasise design.",
+    img: "https://preview.cruip.com/storm/images/feature-tile-icon-02.svg"
+  },
+  {
+    title: "User friendly",
+    text: "A pseudo-Latin text used in web design, layout, and printing in place of things to emphasise design.",
+    img: "https://preview.cruip.com/storm/images/feature-tile-icon-03.svg"
+  },
+  {
+    title: "User friendly",
+    text: "A pseudo-Latin text used in web design, layout, and printing in place of things to emphasise design.",
+    img: "https://preview.cruip.com/storm/images/feature-tile-icon-04.svg"
+  },
+  {
+    title: "User friendly",
+    text: "A pseudo-Latin text used in web design, layout, and printing in place of things to emphasise design.",
+    img: "https://preview.cruip.com/storm/images/feature-tile-icon-05.svg"
+  },
+  {
+    title: "User friendly",
+    text: "A pseudo-Latin text used in web design, layout, and printing in place of things to emphasise design.",
+    img: "https://preview.cruip.com/storm/images/feature-tile-icon-06.svg"
+  }
+]
+</script>
+
+<template>
+  <div class="py-5 flex flex-col md:flex-row md:flex-wrap md:max-w-[1080px] md:px-5 mx-auto border-b-[0.1px] border-line">
+    <div v-for="item in list" class="flex-col text-center max-w-xs mx-auto mb-8 sm:mb-12 p-2">
+      <div class="w-20 h-20 flex justify-center items-center rounded-full bg-icon mx-auto mb-4">
+        <img :src="item.img" alt="1st Icon">
+      </div>
+      <h2 class="text-2xl font-semibold mb-3">{{ item.title }}</h2>
+      <p class="text-gray text-lg">
+        {{ item.text }}
+      </p>
+    </div>
+  </div>
+</template>
